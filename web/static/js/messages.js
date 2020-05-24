5b13db6a799527d190164f19a5788e52be65a719
Jorge <jorge.castillo@utec.edu.pe>
2020-05-23 20:23:46 -0500

    Web

---
+++ web/static/js/messages.js
@@ -0,0 +1,85 @@
+$(function(){
+    var url = "/messages";
+    var users_link = "/users";
+    $("#grid").dxDataGrid({
+        dataSource: DevExpress.data.AspNet.createStore({
+            key: "id",
+            loadUrl: url,
+            insertUrl: url,
+            updateUrl: url,
+            deleteUrl: url,
+            onBeforeSend: function(method, ajaxOptions) {
+                ajaxOptions.xhrFields = { withCredentials: true };
+            }
+        }),
+
+        editing: {
+            editMode: "batch",
+            allowUpdating: true,
+            allowDeleting: true,
+            allowAdding: true
+        },
+
+        paging: {
+            pageSize: 12
+        },
+        sorting: {
+            mode: "multiple"
+        },
+        allowColumnReordering: "true",
+        allowColumnResizing: "true",
+        filterRow: {
+            visible: true
+        },
+        pager: {
+            showPageSizeSelector: true,
+            allowedPageSizes: [8, 12, 20]
+        },
+
+        columns: [{
+            dataField: "id",
+            dataType: "number",
+            allowEditing: false
+        }, {
+            dataField: "content",
+            allowEditing: true
+        }, {
+            dataField: "sent_on",
+            dataType: "date"
+        }, {
+            dataField: "user_from_id",
+            allowEditing: true,
+            lookup: {
+                dataSource: DevExpress.data.AspNet.createStore({
+                    key: "id",
+                    loadUrl : users_link,
+                    insertUrl : users_link,
+                    updateUrl : users_link,
+                    deleteUrl : users_link,
+                    onBeforeSend:function(method, ajaxOptions){
+                        ajaxOptions.xhrFields = {withCredentials: true};
+                    }
+                }),
+                valueExpr: "id",
+                displayExpr: "username"
+            }
+        }, {
+            dataField: "user_to_id",
+            allowEditing: true,
+            lookup: {
+                dataSource: DevExpress.data.AspNet.createStore({
+                    key: "id",
+                    loadUrl: users_link,
+                    insertUrl: users_link,
+                    updateUrl: users_link,
+                    deleteUrl: users_link,
+                    onBeforeSend: function(method, ajaxOptions) {
+                        ajaxOptions.xhrFields = {withCredentials: true};
+                    }
+                }),
+                valueExpr: "id",
+                displayExpr: "username"
+            }
+        } ],
+    }).dxDataGrid("instance");
+});
---
+++ web/static/js/users.js
@@ -0,0 +1,53 @@
+$(function(){
+    var url = '/users'
+    $("#grid").dxDataGrid({
+        dataSource: DevExpress.data.AspNet.createStore({
+            key: "id",
+            loadUrl: url,
+            insertUrl: url,
+            updateUrl: url,
+            deleteUrl: url,
+            onBeforeSend: function(method, ajaxOptions) {
+                ajaxOptions.xhrFields = { withCredentials: true };
+            }
+        }),
+
+        editing: {
+            editMode: "batch",
+            allowUpdating: true,
+            allowDeleting: true,
+            allowAdding: true
+        },
+
+        paging: {
+            pageSize: 12
+        },
+
+        sorting: {
+            mode: "multiple"
+        },
+        allowColumnReordering: "true",
+        allowColumnResizing: "true",
+        filterRow: {
+            visible: true
+        },
+        pager: {
+            showPageSizeSelector: false,
+            allowedPageSizes: [8, 12, 20]
+        },
+
+        columns: [{
+            dataField: "id",
+            dataType: "number",
+            allowEditing: false
+        }, {
+            dataField: "username"
+        }, {
+            dataField: "name"
+        }, {
+            dataField: "fullname"
+        }, {
+            dataField: "password"
+        }, ],
+    }).dxDataGrid("instance");
+});
